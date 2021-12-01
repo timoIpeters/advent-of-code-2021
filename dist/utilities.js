ea1ad244d6b8c8f66b27fd59b02c3e5a4172acdf
Timo Peters <timo.peters1702@gmail.com>
2021-12-01 08:35:18 +0100

    Remove dist folder

--- dist/utilities.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-/* This file bundles some functions in a utility class that could be useful during this years Advent of Code */
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Utility = void 0;
-class Utility {
-}
-exports.Utility = Utility;
